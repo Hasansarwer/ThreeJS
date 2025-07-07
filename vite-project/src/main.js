13074df92d33b1698cc079d223511abfa96f5921
Hasan Sarwer <hasanmathju@gmail.com>
2025-07-07 15:42:45 +0600

    Orbit control added

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls';
 
 const canvas = document.getElementById('canvas');
 
@@ -20,7 +21,7 @@ const dodecahedron = new THREE.Mesh(geometry, material);
 const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
 const boxMaterial = new THREE.MeshLambertMaterial({ color: 0xB4B4B3});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
-box.position.y = -2.5;
+box.position.y = -1.5;
 
 scene.add(dodecahedron);
 scene.add(box);
@@ -33,5 +34,27 @@ scene.add(light);
 // Create a renderer
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 
-renderer.render(scene, camera);
+// Add Orbit Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+
+// Animation loop
+function animate() {
+  requestAnimationFrame(animate);
+  
+  dodecahedron.rotation.x += 0.01;
+  dodecahedron.rotation.y += 0.01;
+
+  box.rotation.y+= 0.005;
+
+
+  controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+  renderer.render(scene, camera);
+}
+animate();
